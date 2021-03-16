1aaeb60af5cd079e80a7fb189fd2bb4af5f56ef4
stick-h <68358227+stick-h@users.noreply.github.com>
2021-03-16 13:52:06 -0400

    Update trigger.js

--- src/commands/trigger.js
+++ src/commands/trigger.js
@@ -46,7 +46,7 @@ module.exports = {
 			return msg.channel.send(`trigger ${name} removed`);
 		}
 		
-		if(isNaN(args[1]) || !config.triggers[args[1]]) return msg.channel.send("invalid index");
+		if(isNaN(args[0]) || !config.triggers[args[0]]) return msg.channel.send("invalid index");
 		if(args[1] != "text" && args[1] != "file" && args[1] != "reaction") return msg.channel.send("invalid arguments");
 		
 		var response = msg.content.split(" ").slice(3).join(" ");
