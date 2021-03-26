a669beee026c8b6ec0ade73e29f51b74499b7080
stick-h <68358227+stick-h@users.noreply.github.com>
2021-03-25 23:33:11 -0400

    Update mongoose.js

--- src/util/mongoose.js
+++ src/util/mongoose.js
@@ -4,10 +4,9 @@ module.exports = {
 	init: () => {
 		mongoose.connect(`mongodb+srv://stick:${process.env.password}@cluster0.jmjsh.mongodb.net/db0?retryWrites=true&w=majority`, {useNewUrlParser: true, useUnifiedTopology: true});
 		mongoose.set("useFindAndModify", false);
-		mongoose.Promise = global.Promise;
 		
-		mongoose.connection.on("connected", () => { console.log("mongoose connected"); });
-		mongoose.connection.on("err", err => { console.error(`mongoose connection error: \n${err.stack}`); });
-		mongoose.connection.on("disconnected", () => { console.warn("mongoose connection lost"); });
+		mongoose.connection.on("connected", () => console.log("mongoose connected"));
+		mongoose.connection.on("err", err => console.error(`mongoose connection error: \n${err.stack}`));
+		mongoose.connection.on("disconnected", () => console.warn("mongoose connection lost"));
 	}
 }
