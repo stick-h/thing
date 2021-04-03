56f958d55d85cc1bd6e143dbb2cca63910b29211
stick-h <68358227+stick-h@users.noreply.github.com>
2021-04-03 18:37:18 -0400

    Update clear.js

--- src/commands/clear.js
+++ src/commands/clear.js
@@ -5,15 +5,15 @@ module.exports = {
 	categ: "Mod",
 	mod: true,
 	run: async(Discord, client, msg, args, config, mod) => {
-		if(isNaN(args[0]*1)) return msg.channel.send("invalid arguments");
+		if(isNaN(args[0])) return msg.channel.send("invalid arguments");
 		if(!mod) return msg.channel.send("mod role required to use this command");
 		
 		const self = msg.guild.members.cache.get(client.user.id);
 		if(!msg.channel.permissionsFor(self).has("MANAGE_MESSAGES")) return msg.channel.send("missing permission: manage messages");
 		if(!msg.channel.permissionsFor(self).has("READ_MESSAGE_HISTORY")) return msg.channel.send("missing permission: read message history");
 		
-		args[0] *= 1;
 		if(!msg.mentions.users.first()){
+			if(args[0] > 1 && args[0] < 100) await msg.delete(); 
 			msg.channel.bulkDelete(args[0]).then(msgs => {
 				msg.channel.send(`${msgs.size} messages deleted`).then(dmsg => setTimeout(() => dmsg.delete(), 3000));
 			}).catch(err => msg.channel.send(`an error has occured\n\`${err}\``));
