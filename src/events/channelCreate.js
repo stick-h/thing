bcf657fe435b40c12bc3685b79a71cae4aedb7c9
stick-h <68358227+stick-h@users.noreply.github.com>
2021-04-14 17:49:11 -0400

    Create channelCreate.js

---
+++ src/events/channelCreate.js
@@ -0,0 +1,9 @@
+module.exports = async(mongoose, Guild, client, channel) => {
+	channel.guild.fetchAuditLogs().then(log => {
+		const entry = log.entries.find(entry => entry.target.id == channel.id);
+		if(entry.target.parent.id == "813868386231517184") entry.target.overwritePermissions([
+			{"id": entry.executor.id, "allow": ["MANAGE_CHANNELS", "MANAGE_ROLES", "SEND_MESSAGES", "MANAGE_MESSAGES", "MUTE_MEMBERS", "DEAFEN_MEMBERS", "MOVE_MEMBERS"]},
+			{"id": channel.guild.roles.everyone.id, "deny": ["MANAGE_CHANNELS"]}
+		]).catch(err => console.log("unable to update permissions"));
+	});
+}
