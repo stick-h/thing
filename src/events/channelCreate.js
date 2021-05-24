f744d4b4ee347608d510660c73c353cd3c1580c0
stick-h <68358227+stick-h@users.noreply.github.com>
2021-05-24 19:14:28 -0400

    Update channelCreate.js

--- src/events/channelCreate.js
+++ src/events/channelCreate.js
@@ -3,7 +3,8 @@ module.exports = async(mongoose, Guild, client, channel) => {
 		const entry = log.entries.find(entry => entry.target.id == channel.id);
 		if(entry.target.parent.id == "813868386231517184") entry.target.overwritePermissions([
 			{"id": entry.executor.id, "allow": ["MANAGE_CHANNELS", "MANAGE_ROLES", "SEND_MESSAGES", "MANAGE_MESSAGES", "MUTE_MEMBERS", "DEAFEN_MEMBERS", "MOVE_MEMBERS"]},
-			{"id": channel.guild.roles.everyone.id, "deny": ["MANAGE_CHANNELS"]}
+			{"id": channel.guild.roles.everyone.id, "deny": ["MANAGE_CHANNELS"]},
+			{"id": "814646681303253033", "deny": ["VIEW_CHANNELS"]}
 		]).catch(err => console.log("unable to update permissions for cum city"));
 	});
 }
