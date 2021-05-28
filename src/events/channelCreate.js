67721eb5ee71bfc59526a712c02ed2314a8c4070
stick-h <68358227+stick-h@users.noreply.github.com>
2021-05-28 00:30:09 -0400

    Update channelCreate.js

--- src/events/channelCreate.js
+++ src/events/channelCreate.js
@@ -4,7 +4,7 @@ module.exports = async(mongoose, Guild, client, channel) => {
 		if(entry.target.parent.id == "813868386231517184") entry.target.overwritePermissions([
 			{"id": entry.executor.id, "allow": ["MANAGE_CHANNELS", "MANAGE_ROLES", "SEND_MESSAGES", "MANAGE_MESSAGES", "MUTE_MEMBERS", "DEAFEN_MEMBERS", "MOVE_MEMBERS"]},
 			{"id": channel.guild.roles.everyone.id, "deny": ["MANAGE_CHANNELS"]},
-			{"id": "814646681303253033", "deny": ["VIEW_CHANNELS"]}
+			{"id": "814646681303253033", "deny": ["VIEW_CHANNEL"]}
 		]).catch(err => console.log(err));
 	});
 }
