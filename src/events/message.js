2b821471b8315255bf74f9f3a9e76b257f5c5411
stick-h <68358227+stick-h@users.noreply.github.com>
2022-01-24 23:12:25 -0500

    Update message.js

--- src/events/message.js
+++ src/events/message.js
@@ -9,12 +9,13 @@ const dir_cmd = path.join(__dirname, "../commands");
 const commands = fs.readdirSync(dir_cmd).filter(file => file.endsWith(".js"));
 
 module.exports = async(mongoose, Guild, client, msg) => {
-	console.log(msg.content);
 	if(!msg.member || msg.author.bot) return;
 	if(!msg.channel.permissionsFor(client.user.id).has("SEND_MESSAGES", true)) return;
 	
 	const command = msg.content.toLowerCase().split(" ")[0];
 	const args = msg.content.toLowerCase().split(" ").slice(1);
+	
+	console.log(msg.content);
 	
 	await Guild.findOne({ guildID: msg.guild.id }).then(config => {
 		if(!config) return console.log(`guild ${msg.guild.name} not found`);
@@ -25,6 +26,8 @@ module.exports = async(mongoose, Guild, client, msg) => {
 			
 			let mod = msg.member.hasPermission("ADMINISTRATOR") ? true : false;
 			config.modroles.forEach(role => { if(msg.member.roles.cache.has(role)) mod = true });
+			
+			console.log(msg.content + "3");
 			
 			let things = [Discord, client, msg, args, config];
 			if(cmd.config) things.push(mongoose);
