d6c6c3167610928ca9ae2f26be516e4578c6126a
stick-h <68358227+stick-h@users.noreply.github.com>
2022-06-18 00:56:37 -0400

    Update nya.js

--- src/commands/nya.js
+++ src/commands/nya.js
@@ -5,7 +5,7 @@ module.exports = {
 	categ: "cats",
 	run: async(Discord, client, msg, args, config) => {
 		const files = [];
-		for(const cat in cats) if(cats[cat].desc) files.push(cats[cat].file);
+		for(const cat in cats) if(cats[cat].file) files.push(cats[cat].file);
 		
 		const r = Math.floor(Math.random()*files.length);
 		msg.channel.send({files: [files[r]]});
