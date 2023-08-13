236921b0543435de6355ac3c56f918902573a154
stick-h <68358227+stick-h@users.noreply.github.com>
2023-08-13 02:21:01 -0400

    Update mongoose.js

--- src/util/mongoose.js
+++ src/util/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 module.exports = {
 	init: () => {
-		mongoose.connect(`mongodb+srv://stick:${process.env.PASSWORD}@cluster0.jmjsh.mongodb.net/db0?retryWrites=true&w=majority`, {useNewUrlParser: true, useUnifiedTopology: true});
+		mongoose.connect(`mongodb+srv://stick:${process.env.PASSWORD}@cluster0.4kup7cd.mongodb.net/db0?retryWrites=true&w=majority`, {useNewUrlParser: true, useUnifiedTopology: true});
 		mongoose.set("useFindAndModify", false);
 		
 		mongoose.connection.on("connected", () => console.log("mongoose connected"));
