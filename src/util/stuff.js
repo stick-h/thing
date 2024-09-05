f9897b8331b83127de2385aa1ae5bb69c3f6dfa3
stick-h <68358227+stick-h@users.noreply.github.com>
2024-09-04 22:02:43 -0400

    better/simpler str detect (#2)

--- src/util/stuff.js
+++ src/util/stuff.js
@@ -5,39 +5,34 @@ const gnns = fs.readdirSync(`${__dirname}/gnn`).filter(file => file.endsWith(".j
 module.exports = {
 	name: "stuff.js",
 	run: async(Discord, client, msg, config) => {
-		function detect(str, obj){
-			let found = 0;
+		function detect(str){
+			let found;
+			str = " " + str + " ";
 			
 			if(str == str.replace(/\W|_/g, " ")){
 				str = " " + str + " ";
-				found = (msg0.content.indexOf(str) != -1) ? true : false;
+				found = (msg0.indexOf(str) != -1) ? true : false;
 			}else found = (msg.content.indexOf(str) != -1) ? true : false;
 			
-			if(found){
-				if(cooldown.bool(msg.member.id, "stuff")) return;
-				if(obj.text || obj.file){
-					if(!obj.file) msg.channel.send(obj.text);
-					else msg.channel.send(obj.text, {files: [obj.file]});
-					cooldown.count(msg.member.id, "stuff");
-				}
-				if(obj.reaction){
-					msg.react(obj.reaction);
-					cooldown.count(msg.member.id, "stuff");
-				}
-				if(str == "gnn"){
-					msg.channel.send({files: [obj]});
-					cooldown.count(msg.member.id, "stuff");
-				}
-			}
+			if(found) cooldown.count(msg.member.id, "stuff");
+			return found;
 		}
 		
+		if(cooldown.bool(msg.member.id, "stuff")) return;
 		msg.content = msg.content.toLowerCase();
-		if(msg.content.includes("thank") && (msg.content.includes("thing") || msg.mentions.users.find(user => user.id == client.user.id))) msg.react("💜");
-		
-		let msg0 = msg.replace(/\W|_/g, " ");
+		let msg0 = msg.content.replace(/\W|_/g, " ");
 		msg0 = " " + msg0.replace(/( ){1,}/g, " ") + " ";
 		
-		if(config.triggers) config.triggers.forEach(obj => detect(obj.name, obj));
-		detect("gnn", `${__dirname}/gnn/${gnns[Math.floor(Math.random()*gnns.length)]}`);
+		if(config.triggers) config.triggers.forEach(obj => {
+			if(detect(obj.name)){
+				if(obj.file) msg.channel.send(obj.text, {files: [obj.file]});
+				else if(obj.text) msg.channel.send(obj.text);
+				if(obj.reaction) msg.react(obj.reaction);
+			}
+		});
+		
+		if(detect("gnn")) msg.channel.send({files: [`${__dirname}/gnn/${gnns[Math.floor(Math.random()*gnns.length)]}`]})
+		
+		if(msg.content.includes("thank") && detect("thing") || msg.mentions.users.find(user => user.id == client.user.id))) msg.react("💜");
 	}
 }
