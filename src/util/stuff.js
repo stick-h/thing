4c492e231b31f1ae9be614f25c44fa783ffb3876
stick-h <68358227+stick-h@users.noreply.github.com>
2024-09-04 20:49:57 -0400

    better/simplified str detection

--- src/util/stuff.js
+++ src/util/stuff.js
@@ -8,26 +8,24 @@ module.exports = {
 		function detect(str, obj){
 			let found = 0;
 			
-			if(str == str.replace(/\W/g, " ")){
-				let strA = str.split(" ");
-				for(i = 0; i < msgA.length; i++){
-					for(j = 0; j < strA.length; j++) if(msgA[i + j] == strA[j]) found++;
-					if(found == strA.length) i = msgA.length;
-					else found = 0;
-				}
-				found = (found == strA.length) ? true : false;
+			if(str == str.replace(/\W|_/g, " ")){
+				str = " " + str + " ";
+				found = (msg0.content.indexOf(str) != -1) ? true : false;
 			}else found = (msg.content.indexOf(str) != -1) ? true : false;
 			
 			if(found){
-				if(obj.reaction) msg.react(obj.reaction);
+				if(cooldown.bool(msg.member.id, "stuff")) return;
 				if(obj.text || obj.file){
-					if(cooldown.bool(msg.member.id, "stuff")) return;
 					if(!obj.file) msg.channel.send(obj.text);
 					else msg.channel.send(obj.text, {files: [obj.file]});
 					cooldown.count(msg.member.id, "stuff");
-				}else{
-					if(cooldown.bool(msg.member.id, "stuff")) return;
-					if(str == "gnn") msg.channel.send({files: [obj]});
+				}
+				if(obj.reaction){
+					msg.react(obj.reaction);
+					cooldown.count(msg.member.id, "stuff");
+				}
+				if(str == "gnn"){
+					msg.channel.send({files: [obj]});
 					cooldown.count(msg.member.id, "stuff");
 				}
 			}
@@ -36,10 +34,10 @@ module.exports = {
 		msg.content = msg.content.toLowerCase();
 		if(msg.content.includes("thank") && (msg.content.includes("thing") || msg.mentions.users.find(user => user.id == client.user.id))) msg.react("💜");
 		
-		let msgA = msg.content.split(/\W|_/g);
-		for(i = 0; i < msgA.length; i++) if(msgA[i] == ""){msgA.splice(i,1); i--;}
-		if(config.triggers) config.triggers.forEach(obj => detect(obj.name, obj));
+		let msg0 = msg.replace(/\W|_/g, " ");
+		msg0 = " " + msg0.replace(/( ){1,}/g, " ") + " ";
 		
+		if(config.triggers) config.triggers.forEach(obj => detect(obj.name, obj));
 		detect("gnn", `${__dirname}/gnn/${gnns[Math.floor(Math.random()*gnns.length)]}`);
 	}
 }
